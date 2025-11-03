065ea6332c832843918fb0655ac6b4fb2c38ed93
Pushpa L <pushpapushpa0630@gmail.com>
2025-11-03 14:33:45 +0530

    Create pointer 1.c

---
+++ pointer 1.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+    int *ptr ,a;
+    ptr=&a;
+    *ptr=a;
+    printf("enter the value of a\n");
+    scanf("%d",&a);
+    printf("%p",ptr);
+    printf(" \nthen the value of a is :%d",*ptr);
+}
